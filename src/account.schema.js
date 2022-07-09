c007d70b7669024295a2e220cd8a5e911dc8bd82
Hiekki <72362116+Hiekki4@users.noreply.github.com>
2022-07-09 13:59:12 +0100

    name field

--- src/account.schema.js
+++ src/account.schema.js
@@ -5,4 +5,5 @@ module.exports = updateAccountSchema = object({
     .min(3, 'Username must be between 3 and 32 characters.')
     .max(32, 'Username must be between 3 and 32 characters.'),
   email: string().email('Must be a valid email address.'),
+  name: string().max(50, 'First name must be 50 characters or less.'),
 });
